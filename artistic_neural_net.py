aa88f077b36faed228bae6908d7a0a8ce990277b
igreat <goraguy5@gmail.com>
2022-07-08 18:55:09 -0700

    Make run on m1 gpu + save images + some quality improvements

--- artistic_neural_net.py
+++ artistic_neural_net.py
@@ -1,19 +1,22 @@
 # TODO: Make the code more modularized (split functionality over files where possible)
-# TODO: Implement it with VGG networks or allow
-#  functionality to switch between networks more easily
+# TODO: Make a video rendering functionality after cleaning up code
 
-import torch
-from torchvision.models import vgg19
-from torchvision.models.feature_extraction import get_graph_node_names
-from torchvision.models.feature_extraction import create_feature_extractor
-from torchvision import transforms
-import torch.nn as nn
-import torch.optim as optim
-from PIL import Image
 import matplotlib.pyplot as plt
+from PIL import Image
+import torch.optim as optim
+import torch.nn as nn
+from torchvision import transforms
+from torchvision.models import vgg19, VGG19_Weights
+from torchvision.models.feature_extraction import create_feature_extractor
+from torchvision.utils import save_image
+import torch
+# from torchvision.models.feature_extraction import get_graph_node_names
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = {torch.has_cuda: "cuda",
+          torch.has_mps: "mps"}.get(True, "cpu")
+
 print(f"Using {device} device")
+
 
 def content_loss(featmaps_gen, featmaps_target):
     return nn.functional.mse_loss(featmaps_gen, featmaps_target, reduction="sum")
@@ -31,41 +34,45 @@ def get_gram_matrix(featmaps):
 
 
 # # loading pretrained model
-model = vgg19(pretrained=True).eval().to(device)
-# print(model)
-# print(get_graph_node_names(model))
+model = vgg19(weights=VGG19_Weights.DEFAULT).eval().to(device)
+
 model.requires_grad = False
-style_nodes = ["features.0", "features.5", "features.10", "features.19", "features.28"]
+style_nodes = ["features.0", "features.5",
+               "features.10", "features.19", "features.28"]
 
 # # setting up feature extractors
-style_feature_extractor = create_feature_extractor(model, return_nodes=style_nodes).to(device)
-content_feature_extractor = create_feature_extractor(model, return_nodes=["features.21"]).to(device)
+style_feature_extractor = create_feature_extractor(
+    model, return_nodes=style_nodes).to(device)
+content_feature_extractor = create_feature_extractor(
+    model, return_nodes=["features.21"]).to(device)
 
 # loading test images
-night = Image.open("night.jpg")
-house = Image.open("house.jpg")
+scream = Image.open("images/the-scream.jpg")
+ship = Image.open("images/ship.jpg")
 
-image_size = 512 # make sure both images have the same dimensions
+image_size = 256  # make sure both images have the same dimensions
 
-vgg_mean_255 = torch.tensor([123.675, 116.28, 103.53]).to(device)
-vgg_std = torch.tensor([1, 1, 1]).to(device)
+vgg_mean_255 = torch.tensor([123.675, 116.28, 103.53])
+vgg_std = torch.tensor([1, 1, 1])
 
 convert_tensor = transforms.Compose([
-    transforms.ToTensor(),
     transforms.Resize((image_size, image_size)),
+    transforms.ToTensor(),
     # experimenting with range 255 (seems to work better for now)
     transforms.Lambda(lambda x: x.mul(255)),
     transforms.Normalize(mean=vgg_mean_255, std=vgg_std)
 ])
 
-night = convert_tensor(night).view(1, 3, image_size, image_size).to(device)
-house = convert_tensor(house).view(1, 3, image_size, image_size).to(device)
+ship = convert_tensor(ship).view(1, 3, image_size, image_size).to(device)
+scream = convert_tensor(scream).view(1, 3, image_size, image_size).to(device)
 
 # initializing the input image to the content image
-gen_image = torch.rand(1, 3, image_size, image_size).to(device).div(255/2) + 255/4
+# gen_image = torch.rand(1, 3, image_size, image_size).to(
+#     device).div(255/2) + 255/4
+gen_image = ship.clone()
 
-content_features_target = content_feature_extractor(house)
-style_features_target = style_feature_extractor(night)
+content_features_target = content_feature_extractor(ship)
+style_features_target = style_feature_extractor(scream)
 
 # making sure that the target content and style features are detached from the computational graph
 for layer in content_features_target.keys():
@@ -81,9 +88,10 @@ for layer in style_features_target.keys():
 
 # setting up for optimization
 content_weight = 1
-style_weight = 1e6
+style_weight = 1e5
 gen_image.requires_grad = True
-optimizer = optim.LBFGS([gen_image], max_iter=2000, line_search_fn='strong_wolfe')
+optimizer = optim.LBFGS([gen_image], max_iter=1000,
+                        line_search_fn='strong_wolfe')
 step_cnt = 0
 
 
@@ -104,9 +112,11 @@ def closure():
     l_content = 0
     # for now, I will let all considered layers contribute equally
     for layer in gram_matrices_target.keys():
-        l_style += style_loss(gram_matrices_gen[layer], gram_matrices_target[layer])
+        l_style += style_loss(gram_matrices_gen[layer],
+                              gram_matrices_target[layer])
     for layer in gen_features_content.keys():
-        l_content += content_loss(gen_features_content[layer], content_features_target[layer])
+        l_content += content_loss(
+            gen_features_content[layer], content_features_target[layer])
 
     with torch.no_grad():
         if step_cnt % 50 == 0:
@@ -126,9 +136,9 @@ optimizer.step(closure)
 # code to display the generated image and compare it with the style and content images
 def display_image(image):
     img = image.clone()
-    # "unnormalizing"
-    img *= vgg_std.view(1, 3, 1, 1)
-    img += vgg_mean_255.view(1, 3, 1, 1)
+    # "denormalizing"
+    img *= vgg_std.view(1, 3, 1, 1).to(device)
+    img += vgg_mean_255.view(1, 3, 1, 1).to(device)
     img = img.div(255)
     img = img.squeeze(0)
     img = img.permute((1, 2, 0))
@@ -139,10 +149,20 @@ def display_image(image):
 gen_image.requires_grad = False
 
 plt.figure()
-display_image(house)
+display_image(ship)
 
 plt.figure()
-display_image(night)
+display_image(scream)
 
 plt.figure()
 display_image(gen_image)
+
+plt.show()
+
+# saving image
+image_to_save = gen_image.clone()
+# denormalizing the image
+image_to_save *= vgg_std.view(1, 3, 1, 1).to(device)
+image_to_save += vgg_mean_255.view(1, 3, 1, 1).to(device)
+image_to_save = image_to_save.div(255)
+save_image(image_to_save, "generated images/screamship.png")
---
+++ models/models.py
@@ -0,0 +1,1 @@
+# Here is where I will set up the feature extractor model
